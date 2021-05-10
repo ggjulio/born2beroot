2190d94be924d96ebd32919405f1903d264b74d5
ggjulio <juligonz@student.42.fr>
2021-05-10 14:08:51 +0200

    .

--- test.py
+++ test.py
@@ -2,50 +2,63 @@ import os
 import hashlib
 import time
 
-paths=["/etc", "/var/log", "/var/spool"]
-paths += os.environ["PATH"].split(sep=':')
-
-paths=["/home/ggjulio/Documents/"]
-paths=["/home/ggjulio/VirtualBox VMs/born2beroot"]
-
 def scantree(path):
-    """Recursively yield DirEntry objects for given directory."""
-    for entry in os.scandir(path):
-        if entry.is_dir(follow_symlinks=False):
-            yield from scantree(entry.path)
-        else:
-            yield entry
+	"""Recursively yield DirEntry objects for given directory."""
+	for entry in os.scandir(path):
+		if entry.is_dir(follow_symlinks=False):
+			yield from scantree(entry.path)
+		else:
+			yield entry
 
-def  hashtrees(paths=[], buf_size=128 * 500):
-	""" Hash every files from all given paths (recursively)"""
+def  hashtrees(paths, buf_size=128 * 500):
+	""" Hash every files from all given paths array (recursively)"""
 	sha1 = hashlib.sha1()
 	read_bytes = 0
 	for path in paths:
-		print(path)
+		print(path+'/')
 		for entry in scantree(path):
 			if entry.is_file():
 				read_bytes += os.stat(entry.path).st_size
 				with open(entry.path, 'rb') as f:
 					while data := f.read(buf_size):
 						sha1.update(data)
-	return read_bytes
+			elif entry.is_dir():
+				print("   "+entry.path+'/')
+
+	return sha1.hexdigest(), read_bytes
+
+def hash_itself():
+	""" Get the hash of the script itself"""
+	sha1 = hashlib.sha1()
+
+	with open(__file__, 'rb') as f:
+		while data := f.read():
+			sha1.update(data)
+	return sha1.hexdigest()
+
+def dupplicate_path(paths):
+	result=False
+	seen = set()
+	for x in paths:
+		if x in seen:
+			print(f"error dupplicate: {x}")
+			result=True
+		seen.add(x)
+	return result
 
 
+
+p=["/etc","/etc", "/var/log", "/var/spool", "/usr/local/sbin", "/var"]
+# p += "/home/ggjulio/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games".split(sep=':')
+p += os.environ["PATH"].split(sep=':')
+#p+=["/var/log"]
 
 start_time = time.time()
+if dupplicate_path(p):
+	exit(1)
+hashe, read_bytes = hashtrees(p)
 
-# hashtrees(paths)
-print(f"{__file__}")
-print(f"--- {time.time() - start_time} seconds for {read_bytes/float(1<<30)}GB ---")
-print(f"SHA1: {sha1.hexdigest()}")
-# myhash = hashlib.sha1("Foxe".encode('utf-8'))
-# print(myhash.hexdigest())
-
-
-# with os.scandir('/etc') as it:
-# 	for entry in it:
-# 		if entry.is_file():
-# 			print(entry)
-# 			with open("/etc/resolv.conf", 'rb') as f:
-# 				while data := f.read(buf_size):
-# 					sha1.update(data)
+print("#" * 40)
+print(f"Script  SHA1: {hash_itself()}")
+print(f"Project SHA1: {hashe}")
+print(f"--- {time.time() - start_time:.2f} seconds for {read_bytes/float(1<<30):.02f}GB ---")
